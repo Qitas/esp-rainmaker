7d17400b99fda64360de83b9a072f480c824ce82
qitas <qitas@qitas.cn>
2022-10-11 10:35:09 +0800

    update

--- examples/gpio/main/app_main.c
+++ examples/gpio/main/app_main.c
@@ -110,7 +110,7 @@ void app_main()
     uint8_t mac[6];
     char DeviceName[16];
     esp_read_mac(mac, ESP_MAC_WIFI_STA);
-    sprintf(DeviceName,"AE-%02x%02x%02x%02x%02x%02x",mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
+    sprintf(DeviceName,"C3-%02x%02x%02x%02x%02x%02x",mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
     ESP_LOGI(TAG, "name %s",DeviceName);
     esp_rmaker_node_t *node = esp_rmaker_node_init(&rainmaker_cfg, title, DeviceName);
     if (!node) {
